32191db7af18e814fbbd1815007396a48d2ea2db
Laura Perez <lauraperez.code@gmail.com>
2015-11-05 17:34:53 -0300

    grafo matriz

--- src/grafoMatrizAdyacencia/GrafoM.java
+++ src/grafoMatrizAdyacencia/GrafoM.java
@@ -1,12 +1,60 @@
 package grafoMatrizAdyacencia;
 
+import listaDoble.Lista;
 
-public class GrafoM {
+
+public class GrafoM implements IGrafoM {
+	
 	private int size;
 	private int cantNodos;
-	private String[][] nodosAdyacentes;
+	private Arco[][] matrizNodosAdyacentes;
+	private boolean[] nodosUsados;
 	
-	// TODO resto
+	@Override
+	public void crearGrafo(int cantMax) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void agregarVertice(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void agregarArista(int origen, int destino) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void eliminarVertice(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void eliminarArista(int origen, int destino) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean esAdyacente(int origen, int destino) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Lista listaAdyacentes(int vertice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean existeVertice(int v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean esConexo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
-	
+	// TODO resto	
 }
--- src/grafoMatrizAdyacencia/IGrafoM.java
+++ src/grafoMatrizAdyacencia/IGrafoM.java
@@ -1,5 +1,24 @@
 package grafoMatrizAdyacencia;
 
+import listaDoble.Lista;
+
 public interface IGrafoM {
 
+	public void crearGrafo(int cantMax);
+
+	public void agregarVertice(int numero);
+
+	public void agregarArista(int origen, int destino);
+
+	public void eliminarVertice(int numero);
+
+	public void eliminarArista(int origen, int destino);
+
+	public boolean esAdyacente(int origen, int destino);
+
+	public Lista listaAdyacentes(int vertice);
+
+	public boolean existeVertice(int v);
+
+	public boolean esConexo();
 }
